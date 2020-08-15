6fc15a727d39ec2f147236c77885743ca8ff5f39
Craig Paul <craig.paul@coconutsoftware.com>
2020-08-15 15:34:14 -0600

    Adds test to extend support for validating the X-Twilio-Signature for GET requests

--- pkg/security_test.go
+++ pkg/security_test.go
@@ -19,6 +19,73 @@ const validSignatureParameters = `FromZip=89449&From=%2B15306666666&` +
 	`Called=%2B15306384866&CallerCountry=US&CalledZip=94612&CallSid=CAd800bb12c0426a7ea4230e492fef2a4f&` +
 	`CallerState=CA&ToCountry=US&ToState=CA`
 
+func TestCanCheckSignatureForGetRequest(t *testing.T) {
+	authToken := "1c892n40nd03kdnc0112slzkl3091j20"
+	twilio := twiligo.Twilio{AuthToken: authToken}
+
+	uri, err := url.Parse("/1ed898x")
+
+	if err != nil {
+		t.Logf("Unexpected error occurred: %s", err)
+		t.Fail()
+	}
+
+	headers := http.Header{
+		"Content-Type":       []string{"application/x-www-form-urlencoded"},
+		"X-Twilio-Signature": []string{"0JNGUhXSEKtzgrpd2HoQz7+F34M="},
+	}
+
+	req := http.Request{
+		Method: "GET",
+		URL:    uri,
+		Header: headers,
+	}
+
+	query, err := url.ParseQuery(string(validSignatureParameters))
+
+	if err != nil {
+		t.Logf("Unexpected error occurred parsing the query parameters: %s", err)
+		t.Fail()
+	}
+
+	req.URL.RawQuery = query.Encode()
+
+	valid, err := twilio.CheckSignature(&req, "http://www.postbin.org")
+
+	if err != nil {
+		t.Logf("Unexpected error occurred while checking the signature: %s", err)
+		t.Fail()
+	}
+
+	if !valid {
+		t.Log("Expected signature to be valid, but was determined to be invalid")
+		t.Fail()
+	}
+
+	headers["X-Twilio-Signature"] = []string{"foo"}
+
+	valid, err = twilio.CheckSignature(&req, "http://www.postbin.org")
+
+	if err != nil {
+		t.Logf("Unexpected error occurred while checking the signature: %s", err)
+		t.Fail()
+	}
+
+	if valid {
+		t.Log("Expected signature to be invalid, but was determined to be valid")
+		t.Fail()
+	}
+
+	delete(headers, "X-Twilio-Signature")
+
+	_, err = twilio.CheckSignature(&req, "http://www.postbin.org")
+
+	if err == nil {
+		t.Log("Expected an error verifying that the given request is missing an X-Twilio-Signature header")
+		t.Fail()
+	}
+}
+
 func TestCanCheckSignatureForPostRequest(t *testing.T) {
 	authToken := "1c892n40nd03kdnc0112slzkl3091j20"
 	twilio := twiligo.Twilio{AuthToken: authToken}
