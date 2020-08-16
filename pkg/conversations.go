5441cbe1002a8bef52fe5a91fc5274381c889fc0
Craig Paul <craig.paul@coconutsoftware.com>
2020-08-16 15:15:48 -0600

    Implements ability to delete an existing conversation

--- pkg/conversations.go
+++ pkg/conversations.go
@@ -125,3 +125,34 @@ func (twilio *Twilio) UpdateConversation(conversationSID string, options Convers
 
 	return response, nil
 }
+
+// DeleteConversation ...
+func (twilio *Twilio) DeleteConversation(conversationSID string) error {
+	req, err := http.NewRequest(http.MethodDelete, twilio.conversationURL("Conversations/"+conversationSID), nil)
+
+	if err != nil {
+		return err
+	}
+
+	req.SetBasicAuth(twilio.credentials())
+
+	res, err := twilio.delete(req)
+
+	if err != nil {
+		return nil
+	}
+
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusNoContent {
+		decoder := json.NewDecoder(res.Body)
+
+		err = new(Exception)
+
+		decoder.Decode(err)
+
+		return err
+	}
+
+	return nil
+}
