c6b1a6688f21f3714af87e27750fc7b000ee490e
Craig Paul <craig.paul@coconutsoftware.com>
2020-08-16 15:10:27 -0600

    Adds initial test cases for updating an existing conversation

--- pkg/conversations_test.go
+++ pkg/conversations_test.go
@@ -38,6 +38,25 @@ const createdConversationResponse = `{
 	}
 }`
 
+const updatedConversationResponse = `{
+	"date_updated": "2020-07-30T00:00:00Z",
+	"friendly_name": "Friendly Conversation",
+	"timers": {},
+	"account_sid": "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	"url": "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	"state": "closed",
+	"date_created": "2020-07-30T00:00:00Z",
+	"messaging_service_sid": "MGXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	"sid": "CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	"attributes": "{}",
+	"chat_service_sid": "ISXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+	"links": {
+		"participants": "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Participants",
+		"messages": "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Messages",
+		"webhooks": "https://conversations.twilio.com/v1/Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX/Webhooks"
+	}
+}`
+
 func TestWillMakeRequestToCreateNewConversationSuccessfully(t *testing.T) {
 	twilio := NewTestTwilio(func(req *http.Request) *http.Response {
 		expected := "Conversations"
@@ -64,7 +83,7 @@ func TestWillMakeRequestToCreateNewConversationSuccessfully(t *testing.T) {
 		}
 	})
 
-	response, err := twilio.CreateNewConversation(twiligo.CreateNewConversationOptions{})
+	response, err := twilio.CreateNewConversation(twiligo.ConversationOptions{})
 
 	if err != nil {
 		t.Logf("Error was incorrectly returned, was not expecting the following error: %s", err)
@@ -127,7 +146,7 @@ func TestWillIncludeProperRequestBodyParametersWhenMakingRequestToCreateNewConve
 		}
 	})
 
-	twilio.CreateNewConversation(twiligo.CreateNewConversationOptions{
+	twilio.CreateNewConversation(twiligo.ConversationOptions{
 		FriendlyName:  "Friendly Conversation",
 		DateCreated:   now,
 		DateUpdated:   now,
@@ -147,7 +166,94 @@ func TestWillHandleErrorResponsesWhenMakingRequestToCreateNewConversation(t *tes
 		}
 	})
 
-	response, err := twilio.CreateNewConversation(twiligo.CreateNewConversationOptions{
+	response, err := twilio.CreateNewConversation(twiligo.ConversationOptions{
+		Attributes: "{\"custom\":\"value}",
+	})
+
+	if response != nil {
+		t.Logf("Response was incorrectly returned, was not expecting the following response: %v", response)
+		t.Fail()
+	}
+
+	expected := "Attributes not valid JSON"
+
+	if err.Error() != expected {
+		t.Logf("Incorrect error returned, expected [%s], but received [%s]", expected, err)
+		t.Fail()
+	}
+}
+
+func TestWillMakeRequestToUpdateConversationSuccessfully(t *testing.T) {
+	twilio := NewTestTwilio(func(req *http.Request) *http.Response {
+		expected := "Conversations/CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+
+		if strings.Contains(req.URL.Path, expected) == false {
+			t.Logf("Incorrect URL supplied, expecting URL to contain [%s], but received [%s]", expected, req.URL.Path)
+			t.Fail()
+		}
+
+		if req.Header.Get("Authorization") == "" {
+			t.Log("Missing authorization credentials, they should be supplied via the Authorization header")
+			t.Fail()
+		}
+
+		if req.Header.Get("Content-Type") != "application/x-www-form-urlencoded" {
+			t.Logf("Incorrect content-type header supplied, expecting [%s], but received [%s]", "application/x-www-form-urlencoded", req.Header.Get("Content-Type"))
+			t.Fail()
+		}
+
+		return &http.Response{
+			Body:       ioutil.NopCloser(bytes.NewBufferString(updatedConversationResponse)),
+			StatusCode: http.StatusOK,
+			Header:     make(http.Header),
+		}
+	})
+
+	response, err := twilio.UpdateConversation("CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", twiligo.ConversationOptions{})
+
+	if err != nil {
+		t.Logf("Error was incorrectly returned, was not expecting the following error: %s", err)
+		t.Fail()
+	}
+
+	if response == nil {
+		t.Log("Did not receive the expected response")
+		t.Fail()
+	}
+}
+
+func TestWillIncludeProperRequestBodyParametersWhenMakingRequestToUpdateConversationSuccessfully(t *testing.T) {
+	twilio := NewTestTwilio(func(req *http.Request) *http.Response {
+		body, _ := ioutil.ReadAll(req.Body)
+		params, _ := url.ParseQuery(string(body))
+
+		if params.Get("State") != "closed" {
+			t.Logf("Incorrect request parameter supplied, expecting [%s], but received [%s]", "active", params.Get("State"))
+			t.Fail()
+		}
+
+		return &http.Response{
+			Body:       ioutil.NopCloser(bytes.NewBufferString(createdConversationResponse)),
+			StatusCode: http.StatusCreated,
+			Header:     make(http.Header),
+		}
+	})
+
+	twilio.CreateNewConversation(twiligo.ConversationOptions{
+		State: "closed",
+	})
+}
+
+func TestWillHandleErrorResponsesWhenMakingRequestToUpdateConversation(t *testing.T) {
+	twilio := NewTestTwilio(func(req *http.Request) *http.Response {
+		return &http.Response{
+			Body:       ioutil.NopCloser(bytes.NewBufferString(attributesAreJsonResponse)),
+			StatusCode: http.StatusBadRequest,
+			Header:     make(http.Header),
+		}
+	})
+
+	response, err := twilio.UpdateConversation("CHXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", twiligo.ConversationOptions{
 		Attributes: "{\"custom\":\"value}",
 	})
 
